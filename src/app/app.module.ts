b3e74e5da493632c50c728ca20a46f3f0ce0660c
Giovanni Vargas <jonan-vargas23@hotmail.com>
2019-12-25 18:17:15 -0500

    Se crearon rutas y hash en el app-routing.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// Routes
 import { AppRoutingModule } from './app-routing.module';
+
+// Components
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { SearchComponent } from './components/search/search.component';
