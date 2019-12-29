3d98d3b65fc4fead8e27d846c534b7175005040b
Giovanni Vargas <jonan-vargas23@hotmail.com>
2019-12-29 16:29:22 -0500

    Se agrego logica de pipe para noimage y se llamo el pipe en el component home y search

--- src/app/pipes/noimage.pipe.ts
+++ src/app/pipes/noimage.pipe.ts
@@ -5,8 +5,13 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class NoimagePipe implements PipeTransform {
 
-  transform(value: any, ...args: any[]): any {
-    return null;
+  transform(images: any[]): string {
+    if(images.length > 0) {
+      return images[0].url;
+    }
+    else {
+      return 'assets/img/noimage.png';
+    }
   }
 
 }
